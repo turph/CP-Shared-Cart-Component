5a495625bbf05b17e9edadf47d40af08ea42dded
Tom Murphy <tom@turphdesigns.com>
2017-11-09 09:30:16 -0700

    Initial
    First

---
+++ js/includes.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+// Order Confirmation
+	$(".sideNav").load('includes/sideNav.html');
+});
---
+++ js/min/includes-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".sideNav").load("includes/sideNav.html")});
